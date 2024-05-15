b25ace8fb45ba343ef5fd2ceb19d1f3a76223520
John M. Bauer <161013370+jmbauer3@users.noreply.github.com>
2024-05-15 12:07:48 +0200

    first!

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("learn-more-button").addEventListener("click", function() {
+    document.getElementById("map-container").classList.toggle("hidden");
+    // Here you can add code to initialize your interactive map
+});
